7f43ac1864c6bef2e1f2351825433acc15239560
Erblin Krasniqi <erblini.kr@gmail.com>
2024-04-25 12:57:12 +0200

    Created hero section

--- blogspace-ui/src/index.js
+++ blogspace-ui/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Assets/css/bootstrap-icons.css";
+import "./Assets/css/templatemo-tiya-golf-club.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

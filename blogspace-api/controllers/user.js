25ffdb07b46e6865a2756c4afd74db49bd10a969
Erblin Krasniqi <erblini.kr@gmail.com>
2024-04-26 01:37:59 +0200

    Created login & register api

--- blogspace-api/controllers/user.js
+++ blogspace-api/controllers/user.js
@@ -1,5 +1,69 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-exports.register = (req, res, next) => {
+exports.register = async (req, res, next) => {
   const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+
+  try {
+    const isSame = await User.findOne({ email: email });
+    if (isSame) {
+      const error = new Error("User already exists 🤔");
+      error.statusCode = 400;
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      email: email,
+      name: name,
+      password: hashedPw,
+    });
+
+    const results = await user.save();
+
+    res.status(201).json({ message: "User created 😁", userId: results._id });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let loadedUser;
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      const error = new Error("No user found 🤔");
+      error.statusCode = 400;
+      throw error;
+    }
+    loadedUser = user;
+
+    const doesMatch = await bcrypt.compare(password, user.password);
+
+    if (!doesMatch) {
+      const error = new Error("Wrong password ⛔");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        email: loadedUser.email,
+        userId: loadedUser._id,
+        userName: loadedUser.name,
+      },
+      "superdupersecret"
+    );
+
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch (err) {
+    next(err);
+  }
 };
